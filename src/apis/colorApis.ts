eff8d25f414e0deb9cb68d517985a25f5db1ab0e
Allen Sarmiento <allensarmiento3@gmail.com>
2025-09-21 20:25:08 -0700

    Add retry mechanism

--- src/apis/colorApis.ts
+++ src/apis/colorApis.ts
@@ -3,14 +3,23 @@ import type {Color} from "../types/color.ts";
 
 const cache: Record<string, Color> = {};
 
-export const getColor = (h: number, s: string, l: string): Promise<Color> => {
+export const getColor = async (h: number, s: string, l: string): Promise<Color> => {
+	let retryCount = 3;
+
 	const key = `hsl(${h},${s},${l})`;
 	if (cache[key]) {
 		return Promise.resolve({ ...cache[key], fromCache: true });
 	}
 
-	return colorService.get(`/id?hsl=${key}`).then(({ data }) => {
-		cache[key] = data;
-		return { ...data, fromCache: false };
-	});
+	while (retryCount > 0) {
+		try {
+			const response = await colorService.get(`/id?hsl=${key}`);
+			cache[key] = response.data;
+			return { ...response.data, fromCache: false };
+		} catch {
+			retryCount--;
+		}
+	}
+
+	return Promise.reject('Failed to get color');
 };
