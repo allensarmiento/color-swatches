113cc7ee76d6fe351d97e11dfda7fc60687cfda3
Allen Sarmiento <allensarmiento3@gmail.com>
2025-09-21 19:16:25 -0700

    Remove unneeded files and add docker file

--- tests/colors.test.ts
+++
@@ -1,8 +0,0 @@
-import {getDistinctColors} from "../src/utils/colors.util";
-import {colors} from "./constants/colors";
-
-test('gets distinct color names', () => {
-	const distinctColorsBinarySearch = getDistinctColors(colors).map(color => color.name.value);
-	const distinctColorsBruteForce = [...new Set(colors.map(color => color.name.value))];
-	expect(distinctColorsBinarySearch).toEqual(distinctColorsBruteForce);
-});
